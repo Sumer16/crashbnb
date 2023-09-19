e85b275461a708b84dd188f70a47aa370caf40f6
Sumer Patel <patelsum@oregonstate.edu>
2023-09-19 00:19:08 -0700

    Added title and changed metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,14 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import { Nunito } from 'next/font/google'
 
-// const inter = Inter({ subsets: ['latin'] })
+const font = Nunito({ subsets: ['latin'] })
 
 // Reserved constant in NextJS 13 to control your metadatas
 export const metadata: Metadata = {
-  title: 'CrashBnB',
-  description: 'This is a CrashBnB clone website developed using NextJS 13.4',
+  title: 'CrashBnB | An AirBnB Clone',
+  description: 'This is a CrashBnB application, a clone of the airbnb website developed using NextJS 13.4',
 }
-
-// Using this instead of inter
-const font = Nunito({ subsets: ['latin'] })
 
 export default function RootLayout({
   children,
